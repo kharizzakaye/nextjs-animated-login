d5430ec9f5e4db0edcbf1e5a10bc8d393db8e715
Khariza Viloria <kharizzakaye@gmail.com>
2025-03-25 20:13:55 +0800

    move styles to module.css

--- src/app/login-component-2/page.tsx
+++ src/app/login-component-2/page.tsx
@@ -4,7 +4,6 @@ import React, { useState } from "react";
 import styles from "./login-component-2.module.css";
 
 export default function Page() {
-
   // Define state to track the current animation
   const [animation, setAnimation] = useState("");
 
@@ -19,41 +18,69 @@ export default function Page() {
   return (
     <div className={`${styles.localWrapper}`}>
       <div className={`${styles.localHtml} `}>
-
         <div className={`wrapper relative w-[35rem] h-[50rem] ${animation}`}>
-
           <div className={`${styles.formContainer} form-container sign-up`}>
             <form action="#">
               <h2 className={`${styles.cardTitle}`}>sign up</h2>
 
-              <div className="form-group">
-                <input type="text" required/>
+              <div className={`${styles.formGroup} form-group`}>
+                <input
+                  type="text"
+                  required
+                  className={`${styles.inputFields}`}
+                />
                 {/* <FontAwesomeIcon icon={faUser} /> */}
                 <label className={`${styles.fieldLabel}`}>username</label>
               </div>
-              
-              <div className="form-group">
-                <input type="email" required/>
+
+              <div className={`${styles.formGroup} form-group`}>
+                <input
+                  type="email"
+                  required
+                  className={`${styles.inputFields}`}
+                />
                 {/* <i className="fas fa-at"></i> */}
                 <label className={`${styles.fieldLabel}`}>email</label>
               </div>
 
-              <div className="form-group">
-                <input type="password" required/>
+              <div className={`${styles.formGroup} form-group`}>
+                <input
+                  type="password"
+                  required
+                  className={`${styles.inputFields}`}
+                />
                 {/* <i className="fas fa-lock"></i> */}
                 <label className={`${styles.fieldLabel}`}>password</label>
               </div>
 
-              <div className="form-group">
-                <input type="password" required/>
+              <div className={`${styles.formGroup} form-group`}>
+                <input
+                  type="password"
+                  required
+                  className={`${styles.inputFields}`}
+                />
                 {/* <i className="fas fa-lock"></i> */}
-                <label className={`${styles.fieldLabel}`}>confirm password</label>
+                <label className={`${styles.fieldLabel}`}>
+                  confirm password
+                </label>
               </div>
 
-              <button type="submit" className="btn">sign up</button>
+              <button type="submit" className={`${styles.buttons} btn`}>
+                sign up
+              </button>
 
-              <div className="link">
-                <p>You already have an account?<a href="#" className="signin-link" onClick={handleSignInClick}> sign in</a></p>
+              <div className={`link text-center text-[1.4rem] text-[var(--labelColor)] my-10`}>
+                <p>
+                  You already have an account?
+                  <a
+                    href="#"
+                    className="signin-link  capitalize text-[var(--mainColor)] no-underline font-semibold transition-all duration-500 ease-in-out hover:text-[#da4453]"
+                    onClick={handleSignInClick}
+                  >
+                    {" "}
+                    sign in
+                  </a>
+                </p>
               </div>
             </form>
           </div>
@@ -62,33 +89,56 @@ export default function Page() {
             <form action="#">
               <h2 className={`${styles.cardTitle}`}>login</h2>
 
-              <div className="form-group">
-                <input type="text" required/>
+              <div className={`${styles.formGroup} form-group`}>
+                <input
+                  type="text"
+                  required
+                  className={`${styles.inputFields}`}
+                />
                 {/* <FontAwesomeIcon icon={faUser} className="icons"/> */}
                 <label className={`${styles.fieldLabel}`}>username</label>
               </div>
 
-              <div className="form-group">
-                <input type="password" required/>
+              <div className={`${styles.formGroup} form-group`}>
+                <input
+                  type="password"
+                  required
+                  className={`${styles.inputFields}`}
+                />
                 {/* <i className="fas fa-lock"></i> */}
                 <label className={`${styles.fieldLabel}`}>password</label>
               </div>
 
-              <div className="forgot-pass">
-                <a href="#">forgot password?</a>
+              <div className="forgot-pass my-6 -mt-6">
+                <a
+                  href="#"
+                  className="text-[var(--labelColor)] no-underline text-[1.4rem] capitalize transition-all duration-500 ease-in-out hover:text-[var(--mainColor)]"
+                >
+                  forgot password?
+                </a>
               </div>
 
-              <button type="submit" className="btn">login</button>
+              <button type="submit" className={`${styles.buttons} btn`}>
+                login
+              </button>
 
-              <div className="link">
-                <p>Don&apos;t have an account?<a href="#" className="signup-link" onClick={handleSignUpClick}> sign up</a></p>
+              <div className={`link text-center text-[1.4rem] text-[var(--labelColor)] my-10`}>
+                <p>
+                  Don&apos;t have an account?
+                  <a
+                    href="#"
+                    className="signup-link capitalize text-[var(--mainColor)] no-underline font-semibold transition-all duration-500 ease-in-out hover:text-[#da4453]"
+                    onClick={handleSignUpClick}
+                  >
+                    {" "}
+                    sign up
+                  </a>
+                </p>
               </div>
             </form>
           </div>
-
-          </div>
-
+        </div>
       </div>
     </div>
   );
-};
+}
