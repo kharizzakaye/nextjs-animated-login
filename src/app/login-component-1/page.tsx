d5875ae6a8d9fd55181d1e0fdbebc71ce960d9d9
Khariza Viloria <kharizzakaye@gmail.com>
2025-03-25 12:28:44 +0800

    update button click handlers

--- src/app/login-component-1/page.tsx
+++ src/app/login-component-1/page.tsx
@@ -4,36 +4,26 @@ import { useEffect } from "react";
 import SocialIconsSection from "../components/SocialIconsSection";
 
 export default function Page() {
-
-  useEffect(() => {
+  const handleRegisterClick = () => {
     const container = document.getElementById("container");
-    const registerBtn = document.getElementById("register");
-    const loginBtn = document.getElementById("login");
-
-    if (registerBtn && loginBtn && container) {
-      registerBtn.addEventListener("click", () => {
-        container.classList.add("active");
-      });
-
-      loginBtn.addEventListener("click", () => {
-        container.classList.remove("active");
-      });
+    if (container) {
+      container.classList.add("active");
     }
+  };
 
-    // Cleanup event listeners when component unmounts
-    return () => {
-      if (registerBtn) registerBtn.removeEventListener("click", () => {});
-      if (loginBtn) loginBtn.removeEventListener("click", () => {});
-    };
-  }, []);
-
+  const handleLoginClick = () => {
+    const container = document.getElementById("container");
+    if (container) {
+      container.classList.remove("active");
+    }
+  };
 
   return (
-    <div 
+    <div
       className="
         m-0 p-0 box-border
         bg-gradient-to-r from-[#e2e2e2] to-[#c9d6ff] flex items-center justify-center flex-col h-screen"
-      >
+    >
       <div className="container" id="container">
         {/* create account container */}
         <div className="formContainer signUp">
@@ -75,26 +65,29 @@ export default function Page() {
         <div className="toggleContainer">
           <div className="toggle">
             {/* left toggle */}
-            <div className= "togglePanel toggleLeft">
+            <div className="togglePanel toggleLeft">
               <h1>Welcome back!</h1>
 
               <p>Enter your personal details to use all of the site features</p>
 
-              <button className="hiddenButton" id="login">Sign in</button>
+              <button className="hiddenButton" id="login" onClick={handleLoginClick}>
+                Sign in
+              </button>
             </div>
 
             {/* right toggle */}
             <div className="togglePanel toggleRight">
-              <h1 >Hi there!</h1>
+              <h1>Hi there!</h1>
 
               <p>Register with your personal details to use our website</p>
 
-              <button className="hiddenButton" id="register">Sign up</button>
+              <button className="hiddenButton" id="register" onClick={handleRegisterClick}>
+                Sign up
+              </button>
             </div>
           </div>
         </div>
-
       </div>
     </div>
-  )
+  );
 }
