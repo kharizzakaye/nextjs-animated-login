78a854c91453aa8e38ad3e66f8d737bf3dde8805
Khariza Viloria <kharizzakaye@gmail.com>
2025-03-25 15:21:25 +0800

    Add temporary links to components

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,14 @@
+import Link from "next/link";
+
 export default function Home() {
   return (
-    <>
-      Hello
-    </>
+    <div className="flex flex-col">
+      <div>
+        <Link href="/login-component-1" >Login Component 1</Link>
+      </div>
+      <div>
+        <Link href="/login-component-2" >Login Component 2</Link>
+      </div>
+    </div>
   )
 }
